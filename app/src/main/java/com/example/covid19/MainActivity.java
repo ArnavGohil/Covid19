ab09a8f3445de118f3e46b857f682a379613d481
Arnav <arnav.gohil04@gmail.com>
2020-03-26 22:50:13 +0530

    SPLASH SCREEN :rocket:

--- app/src/main/java/com/example/covid19/MainActivity.java
+++ app/src/main/java/com/example/covid19/MainActivity.java
@@ -2,7 +2,14 @@ package com.example.covid19;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LottieAnimationView lottieAnimationView = findViewById(R.id.anim);
+        long time = lottieAnimationView.getDuration();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this , HomeActivity.class),
+                        ActivityOptions.makeSceneTransitionAnimation(MainActivity.this).toBundle());
+            }
+        }, 3000 );
     }
 }
