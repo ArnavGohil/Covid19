6795df31488f7c068a924fdf9e5b3e3ae4427bfa
Arnav <arnav.gohil04@gmail.com>
2020-03-27 18:47:18 +0530

    WebView Added :octocat:

--- app/src/main/java/com/example/covid19/HomeActivity.java
+++ app/src/main/java/com/example/covid19/HomeActivity.java
@@ -1,14 +1,39 @@
 package com.example.covid19;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class HomeActivity extends AppCompatActivity {
 
+    FloatingActionButton fab ;
+    FrameLayout frameLayout ;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        fab = findViewById(R.id.fab);
+        frameLayout = findViewById(R.id.frame) ;
+
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(savedInstanceState == null) {
+                    WebFragment webFragment = new WebFragment();
+                    FragmentManager fragmentManager = getSupportFragmentManager();
+                    fragmentManager.beginTransaction()
+                            .add(R.id.frame, webFragment)
+                            .commit();
+                }
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/covid19/WebFragment.java
@@ -0,0 +1,34 @@
+package com.example.covid19;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class WebFragment extends Fragment {
+
+    public WebFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_web, container, false);
+        WebView mywebview = v.findViewById(R.id.web);
+        mywebview.setWebViewClient(new WebViewClient());
+        mywebview.loadUrl("http://www.google.com/");
+        return v ;
+    }
+}
