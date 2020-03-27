caf0afc6aaf5d28c68069a92bf301cacc4af708a
Arnav <arnav.gohil04@gmail.com>
2020-03-27 19:20:02 +0530

    WebView Added :octocat:

--- app/src/main/java/com/example/covid19/WebFragment.java
+++ app/src/main/java/com/example/covid19/WebFragment.java
@@ -1,9 +1,12 @@
 package com.example.covid19;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,9 +29,25 @@ public class WebFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_web, container, false);
-        WebView mywebview = v.findViewById(R.id.web);
+        final WebView mywebview = v.findViewById(R.id.web);
         mywebview.setWebViewClient(new WebViewClient());
         mywebview.loadUrl("http://www.google.com/");
+        mywebview.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+
+                if (keyCode == KeyEvent.KEYCODE_BACK){
+                    if(mywebview.canGoBack())
+                        mywebview.goBack();
+                    else
+                        startActivity(new Intent(getActivity() , HomeActivity.class),
+                                ActivityOptions.makeSceneTransitionAnimation(getActivity()).toBundle());
+                    return true;
+                }
+                return false;
+            }
+        });
+
         return v ;
     }
 }
