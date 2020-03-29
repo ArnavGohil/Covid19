e148bbce199985b5dd84dd4ef46f2f9991400b61
Arnav <arnav.gohil04@gmail.com>
2020-03-29 15:52:13 +0530

    Commit :octocat:

--- app/src/main/java/com/example/covid19/Adapters/ImgAdapter.java
+++ app/src/main/java/com/example/covid19/Adapters/ImgAdapter.java
@@ -1,13 +1,20 @@
 package com.example.covid19.Adapters;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import com.example.covid19.Models.Img;
 import com.example.covid19.R;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.snackbar.Snackbar;
 
 
 import java.util.ArrayList;
@@ -26,8 +33,25 @@ public class ImgAdapter extends ArrayAdapter<Img>
             listItemView = LayoutInflater.from(getContext()).inflate(
                     R.layout.item_prec, parent, false);
         }
+        final Img currentAndroidFlavor = getItem(position);
 
-        Img currentAndroidFlavor = getItem(position);
+        MaterialButton button = listItemView.findViewById(R.id.download) ;
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(currentAndroidFlavor.getID() == R.drawable.questions)
+                {
+                    //TODO Open WhatsApp link here ;
+                    return;
+                }
+                android.content.ClipboardManager clipboard = (android.content.ClipboardManager) getContext().getSystemService(Context.CLIPBOARD_SERVICE);
+                android.content.ClipData clip = android.content.ClipData.newPlainText("Copied Text", currentAndroidFlavor.getURL());
+                clipboard.setPrimaryClip(clip);
+                Snackbar.make(view , "LINK COPIED" , Snackbar.LENGTH_SHORT)
+                        .setTextColor(Color.parseColor("#FFAEEA00"))
+                        .show();
+            }
+        });
 
         ImageView iconView = listItemView.findViewById(R.id.imgView);
         iconView.setImageResource(currentAndroidFlavor.getID());
